eb081efdabc492257af61c05f357197b6adc14e4
Avaer Kazmer <ultimus@gmail.com>
2025-01-29 20:08:33 -0800

    Update src

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,21 @@
-import { Client, IAgentRuntime, elizaLogger } from "@elizaos/core";
-import { FarcasterClient } from "./client";
-import { FarcasterPostManager } from "./post";
-import { FarcasterInteractionManager } from "./interactions";
-import { Configuration, NeynarAPIClient } from "@neynar/nodejs-sdk";
+import { FarcasterAgentClient } from './farcaster-agent-client';
 
-export class FarcasterAgentClient implements Client {
-    name = 'farcaster';
-    config = {};
-
-    client: FarcasterClient;
-    posts: FarcasterPostManager;
-    interactions: FarcasterInteractionManager;
-
-    private signerUuid: string;
-
-    constructor(
-        public runtime: IAgentRuntime,
-        client?: FarcasterClient
-    ) {
-        const cache = new Map<string, any>();
-
-        this.signerUuid = runtime.getSetting("FARCASTER_NEYNAR_SIGNER_UUID")!;
-
-        const neynarConfig = new Configuration({
-            apiKey: runtime.getSetting("FARCASTER_NEYNAR_API_KEY")!,
-        });
-
-        const neynarClient = new NeynarAPIClient(neynarConfig);
-
-        this.client =
-            client ??
-            new FarcasterClient({
-                runtime,
-                ssl: true,
-                url:
-                    runtime.getSetting("FARCASTER_HUB_URL") ??
-                    "hub.pinata.cloud",
-                neynar: neynarClient,
-                signerUuid: this.signerUuid,
-                cache,
-            });
-
-        elizaLogger.info("Farcaster Neynar client initialized.");
-
-        this.posts = new FarcasterPostManager(
-            this.client,
-            this.runtime,
-            this.signerUuid,
-            cache
-        );
-
-        this.interactions = new FarcasterInteractionManager(
-            this.client,
-            this.runtime,
-            this.signerUuid,
-            cache
-        );
-    }
-
-    async start() {
-        await Promise.all([this.posts.start(), this.interactions.start()]);
-    }
-
-    async stop() {
-        await Promise.all([this.posts.stop(), this.interactions.stop()]);
-    }
-}
+const FarcasterClientInterface = {
+    name: 'farcaster',
+    config: {},
+    start: async (runtime: any) => {
+        const farcasterAgentClient = new FarcasterAgentClient(runtime);
+        await farcasterAgentClient.start();
+        return farcasterAgentClient as any;
+    },
+    stop: async (runtime: any) => {
+        try {
+            // stop it
+            console.log("Stopping farcaster client", runtime.agentId);
+            await runtime.clients.farcaster.stop();
+        } catch (e) {
+            console.error("client-farcaster interface stop error", e);
+        }
+    },
+};
+export default FarcasterClientInterface;
